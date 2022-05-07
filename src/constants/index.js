0695e917308b9ceab236747d7610cca51f92bd7a
Ouaklim Othmane <ouaklimothmane@gmail.com>
2022-05-07 23:22:26 +0000

    constants file added

---
+++ src/constants/index.js
@@ -0,0 +1,12 @@
+export const API_KEY = "AIzaSyBgyTdL0SDafMvM7_awazEjEuAWlPZ8iio";
+
+export const URL_UPDATE_PASSWORD =
+  "https://identitytoolkit.googleapis.com/v1/accounts:update?key=";
+
+export const URL_SIGN_IN =
+  "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=";
+
+export const URL_SIGN_UP =
+  "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=";
+
+export const HEADERS = { "Content-Type": "application/json" };
