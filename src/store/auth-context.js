5eaff33eeedfda242566a90a41d349351c48208e
Ouaklim Othmane <ouaklimothmane@gmail.com>
2022-05-08 17:27:01 +0100

    Presisting the user authentication status

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -8,15 +8,18 @@ const AuthContext = React.createContext({
 });
 
 export const AuthContextProvider = (props) => {
-  const [token, setToken] = useState(null);
+  const initialToken = localStorage.getItem("token");
+  const [token, setToken] = useState(initialToken);
 
   const userIsLoggedIn = !!token;
 
   const loginHandler = (token) => {
+    localStorage.setItem("token", token);
     setToken(token);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem("token");
     setToken(null);
   };
 
